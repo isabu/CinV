9948bec208daa9be069ca08b3525277b61e03968
isabu <inyakisalinas@gmail.com>
2017-02-10 16:52:04 -0300

    Corregido bug que venía en código de pruebas HelloWorldtest siguiendo las indicaciones en http://struts.1045723.n5.nabble.com/Blank-app-fails-to-build-td5720400.html

--- CV/src/test/java/net/cinv/example/HelloWorldTest.java
+++ CV/src/test/java/net/cinv/example/HelloWorldTest.java
@@ -27,7 +27,7 @@ import org.apache.struts2.StrutsTestCase;
 public class HelloWorldTest extends StrutsTestCase {
 
     public void testHelloWorld() throws Exception {
-        HelloWorld hello_world = new HelloWorld();
+    	HelloWorld hello_world = container.inject(HelloWorld.class); //Bug fix (http://struts.1045723.n5.nabble.com/Blank-app-fails-to-build-td5720400.html)
         String result = hello_world.execute();
         assertTrue("Expected a success result!",
                 ActionSupport.SUCCESS.equals(result));
